5942d72c7833623118f7f38cbc354df32e5e95cb
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2026-03-01 23:15:11 -0300

    Create download.py for Hugging Face snapshot download
    Add script to download snapshots from Hugging Face hub

---
+++ SDumont/download.py
@@ -0,0 +1,12 @@
+import os
+from huggingface_hub import snapshot_download
+
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+
+snapshot_download(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    local_dir="/scratch/unioeste/hefesto/fabricio.tanquella/DeepSeek-R1",
+    local_dir_use_symlinks=False,
+    max_workers=8,
+    token="COLE_SEU_TOKEN_AQUI"
+)
